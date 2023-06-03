278437f8c80c1b334f9a9c863dfd89696f3dad73
Rene A Larson <122948946+ReneLarson1990@users.noreply.github.com>
2023-06-03 10:32:46 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -20,10 +21,17 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
+
 
 
 
